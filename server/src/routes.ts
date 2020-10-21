5c07897723eec04af813ce61a2e1fb2d64ef40f6
SouzaJandison <jandsonbarbosa.dev@gmail.com>
2020-10-20 21:15:23 -0300

    server and web

---
+++ server/src/routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
+import orphanagesController from './controllers/OrphanagesController';
+
+const router = Router();
+const upload = multer(uploadConfig);
+
+router.get('/orphanages', orphanagesController.index);
+router.get('/orphanages/:id', orphanagesController.show)
+router.post('/orphanages', upload.array('images'), orphanagesController.create);
+
+export { router };
